dc6b444b067699600519f16d1779b84410ca085c
Hector Jimenez <hjmontemayor@gmail.com>
2020-08-20 00:11:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
-    puts 
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
 end
